791f495f897cdae8497072bf68e935176c03d576
Ragib Asif <>
2024-05-12 19:40:13 -0400

    reorder files to fix issue with github pages only displaying the readme

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    // res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile('index.html');
 });
 
 app.listen(PORT, () => {
