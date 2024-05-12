4a2f1d89eec8c36e27878a090a2677579009f6b0
Ragib Asif <>
2024-05-12 19:08:54 -0400

    Created node server and created index.html

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path')
+
+const app = express();
+
+// const hostname = '127.0.0.1';
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
