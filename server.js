d7ef7eef0e3ab5b336692500d1be7aed074a5d62
Ragib Asif <>
2024-05-14 15:56:20 -0400

    update server script to fix bug

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    res.sendFile('index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
